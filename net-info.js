4742fc5e4e0ce44096d305faea76aef3ff19d3b9
parchil <70421106+parchil@users.noreply.github.com>
2021-11-09 13:59:14 +0800

    Update net-info.js

--- net-info.js
+++ net-info.js
@@ -64,7 +64,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
         `節點 ISP : ${info.isp}\n` +
         `節點位置 : ${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city
         }`,
-      icon: wifi.ssid ? 'wifi' : 'simcard',
+      icon: wifi.ssid ? 'wifi.circle' : 'simcard',
       'icon-color': wifi.ssid ? '#005CAF' : '#F9BF45',
     });
   });
